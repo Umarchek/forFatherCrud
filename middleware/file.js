2d0b7b3cc3f326170add51b4326e6ff1c2dea8f6
Umarjon <89200068+UZBase@users.noreply.github.com>
2021-10-08 18:11:54 +0300

    middleware chut chut ostalos

---
+++ middleware/file.js
@@ -0,0 +1,19 @@
+const multer = require('multer')
+const moment = require('moment')
+
+// storage filename // allowedTypes
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        const date = moment().format("DDMMYYYY-HHmmss")
+        cb(null, `${date}-${file.filename}`)
+    }
+})
+const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg']
+
+const fileFilter = (req, file, cb) => {
+    if (allowedTypes.includes(file))
+}
