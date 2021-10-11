5bdd9cf48df4697c9edcd145c5c2fccd9863d892
UZBase <sharofiddinov.07@mail.ru>
2021-10-11 18:58:25 +0300

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,13 @@ const router = Router()
 router.get('/product', (req, res) => {
     res.render('admin/product', {
         layout: 'admin',
-        title: 'Car models',
+        title: 'Product models',
+    })
+})
+router.get('/products/add', (req, res) => {
+    res.render('admin/addProducts', {
+        layout: 'admin',
+        title: 'Product models',
     })
 })
 
