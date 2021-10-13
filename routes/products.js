dd65a420a8feaa3d6bfbcc1bcafd5ed5d8bd0eab
UZBase <sharofiddinov.07@mail.ru>
2021-10-13 18:50:28 +0500

    commit!

--- routes/products.js
+++ routes/products.js
@@ -1,31 +1,33 @@
-const {
-    Router
-} = require('express')
-const Category = require('../models/Category')
-const Product = require('../models/Product')
-const router = Router()
-const fileMiddleware = require('../middleware/fileMiddleware')
-router.get('/products', (req, res) => {
-    res.render('admin/products', {
-        layout: 'admin',
-        title: 'Maxsulotlar sahifasi'
-    })
-})
+const { Router } = require("express");
+const Category = require("../models/Category");
+const Product = require("../models/Product");
+const router = Router();
+const fileMiddleware = require("../middleware/fileMiddleware");
+router.get("/products", (req, res) => {
+  res.render("admin/products", {
+    layout: "admin",
+    title: "Maxsulotlar sahifasi",
+  });
+});
 
-router.get('/products/add', async (req, res) => {
-    const categories = await Category.find()
-    res.render('admin/addProduct', {
-        layout: 'admin',
-        title: 'Maxsulotlar sahifasi',
-        categories
-    })
-})
-router.post('/products/add', fileMiddleware.single('img'), async (req, res) => {
-    
-    const product = new Product.find({
-        name: req.body.name,
-        price: req.body.price
-    })
-})
+router.get("/products/add", async (req, res) => {
+  const categories = await Category.find();
+  res.render("admin/addProduct", {
+    layout: "admin",
+    title: "Maxsulotlar sahifasi",
+    categories,
+  });
+});
+router.post("/products/add", fileMiddleware.single("img"), async (req, res) => {
+  const img = req.file.filename;
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    img,
+    categoryId: req.body.categoryId,
+  });
+  await product.save();
+  res.redirect("/admin/products");
+});
 
-module.exports = router
+module.exports = router;
