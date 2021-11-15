ec1c66430c96841308408ac92ac8c7ddac9bcd08
UZBase <sharofiddinov.07@mail.ru>
2021-11-15 16:34:55 +0300

    1

--- routes/category.js
+++ routes/category.js
@@ -61,6 +61,7 @@ router.get('/read/:id', async (req, res) => {
     // res.send(products[0].mahsulotlar)
     products = products[0].mahsulotlar
 
+    console.log(products)
     res.render('admin/category', {
         header: categoryName,
         products,
